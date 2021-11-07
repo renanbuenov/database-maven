c515384b59447f2c4d5d19a39b0375b7b343b20d
Renan Bueno <renanbuenov@hotmail.com>
2021-11-07 16:06:37 -0300

    Class VeiculoService fixed and update

--- src/main/java/dev/renan/VeiculoService.java
+++ src/main/java/dev/renan/VeiculoService.java
@@ -1,8 +1,8 @@
 package dev.renan;
 
+import javax.swing.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 
 public class VeiculoService {
 
@@ -12,52 +12,8 @@ public class VeiculoService {
         mysql = new MySQL();
     }
 
-
-    public static void main(String[] args) {
-        try {
-            VeiculoService veiculoService = new VeiculoService();
-            veiculoService.conectar();
-
-            //veiculoService.inserir();
-
-
-            //veiculoService.inserir();
-
-
-
-            Veiculo pessoa = new Veiculo();
-            /*
-            veiculoService.setNome();
-            veiculoService.setModelo();
-            veiculoService.setPlaca();
-            veiculoService.inserir();
-            */
-
-            //veiculoService.consultar();
-
-            /*
-            veiculo.setNome();
-            veiculo.setModelo();
-            veiculo.setPlaca();
-
-            veiculoService.alterar();
-            veiculoService.consultar();
-            */
-
-            template.excluir(1);
-            template.consultar();
-
-
-
-            //template.mysqlr.stmt.close();
-            //template.mysqlr.conn.close();
-        } catch (Exception e) {
-            System.out.println("Exception: " + e);
-        }
-    }
-
     public void conectar(){
-        boolean connected = mysql.connect("localhost", "8080", "Concessionaria", "root", "1234");
+        boolean connected = mysql.connect("localhost", "3306", "Concessionaria", "root", "1234");
         if (connected) {
             System.out.println("Base de dados conectada.");
         } else {
@@ -65,31 +21,10 @@ public class VeiculoService {
         }
     }
 
-    public void inserir(){
-        String query = "INSERT INTO Veiculo (nome, modelo, placa) "
-                + "values ('Honda Civic', 'Hond', 'ABCD1234')";
-        int status = mysql.executeUpdate(query);
-        if (status == 1) {
-            System.out.println("Dados inseridos com sucesso!");
-        } else {
-            System.out.println("Erro ao inserir dados!");
-        }
-    }
-
-    public void inserir(String nome, String modelo, String placa){
-        String query = "INSERT INTO Pessoa (nome, email, cidade) "
-                + "values ('"+nome+"', '"+modelo+"', '"+placa+"')";
-        int status = mysql.executeUpdate(query);
-        if (status == 1) {
-            System.out.println("Dados inseridos com sucesso!");
-        } else {
-            System.out.println("Erro ao inserir dados!");
-        }
-    }
-
     public void inserir(Veiculo veiculo){
-        String query = "INSERT INTO Pessoa (nome, modelo, placa) "
-                + "values ('"+veiculo.getNome()+"', '"+veiculo.getModelo()+"', '"+veiculo.getPlaca()+"')";
+        String query = "INSERT INTO Veiculo (nome, modelo, placa, ano, preco) "
+                + "values ('"+veiculo.getNome()+"', '"+veiculo.getModelo()+"', '"+veiculo.getPlaca()+
+                "', '"+veiculo.getAno()+"', '"+veiculo.getPreco()+"');";
         int status = mysql.executeUpdate(query);
         if (status == 1) {
             System.out.println("Dados inseridos com sucesso!");
@@ -103,13 +38,14 @@ public class VeiculoService {
         if (rs != null) {
             try {
                 while (rs.next()) {
-                    // É possível referenciar a coluna pelo índice
-                    System.out.println("Id: " + rs.getString(1));
-                    // Ou pelo nome
-                    System.out.println("Nome: " + rs.getString("nome"));
-                    System.out.println("Modelo: " + rs.getString("modelo"));
-                    System.out.println("Placa: " + rs.getString("placa"));
-                    System.out.println();
+
+                    JOptionPane.showMessageDialog(null,
+                            "Nome: "+rs.getString("nome")
+                            +"\nModelo: "+rs.getString("modelo")
+                            +"\nPlaca: "+rs.getString("placa")
+                            +"\nAno: "+rs.getString("ano")
+                            +"\nPreço: "+rs.getString("preco")
+                            );
                 }
             } catch (SQLException e) {
                 System.out.println(e);
@@ -119,9 +55,10 @@ public class VeiculoService {
 
     public void alterar(Veiculo veiculo, int ID){
         int status;
-        String query = "UPDATE Veiculo SET nome='" + veiculo.getNome() + "', "
-                + "nome='" + veiculo.getNome() + "', modelo='" + veiculo.getModelo() + "'  "
-                + "WHERE id=" + ID;
+        String query = "UPDATE Veiculo SET nome='"+veiculo.getNome()+"', "
+                +"modelo='"+veiculo.getModelo()+"', placa='"+veiculo.getPlaca()
+                +"', ano="+veiculo.getAno()+", preco="+veiculo.getPreco()
+                +" WHERE id="+ID+";";
         status = mysql.executeUpdate(query);
         if (status == 1) {
             System.out.println("Dados alterados com sucesso!");
@@ -130,8 +67,8 @@ public class VeiculoService {
         }
     }
 
-    public void excluir(int ID){
-        String query = "DELETE FROM Pessoa WHERE id=" + ID;
+    public void excluir(Integer ID){
+        String query = "DELETE FROM Veiculo WHERE id="+ID+";";
         int status = mysql.executeUpdate(query);
         if (status == 1) {
             System.out.println("Dados excluídos com sucesso!");
