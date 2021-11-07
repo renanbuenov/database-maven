b02d28972eef5e6b03aa7ff038e4f5a72b9c63fb
Renan Bueno <renanbuenov@hotmail.com>
2021-11-06 23:45:12 -0300

    Class MySQL created

---
+++ src/main/java/dev/renan/MySQL.java
@@ -0,0 +1,48 @@
+package dev.renan;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MySQL {
+
+    public Connection conn;
+    public Statement stmt;
+    public ResultSet rs;
+
+    public boolean connect(String address, String port, String dataBaseName, String user, String password) {
+        boolean status = false;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://"+ address + ":" + port +"/"+ dataBaseName + "?user=" + user + "&password=" + password + "&useTimezone=true&serverTimezone=UTC");
+            status = true;
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+
+        }
+        return status;
+    }
+    // Para inserções, alterações e exclusões
+    public int executeUpdate(String query) {
+        int status = 0;
+        try {
+            stmt = conn.createStatement();
+            status = stmt.executeUpdate(query);
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+
+        }
+        return status;
+    }
+
+    // Para consultas
+    public ResultSet executeQuery(String query) {
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(query);
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+        }
+        return rs;
+    }
+}
