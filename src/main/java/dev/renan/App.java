daee7488f7fcac583d6dd57f299584c88a1a78fe
Renan Bueno <renanbuenov@hotmail.com>
2021-11-06 23:27:10 -0300

    Project Started

---
+++ src/main/java/dev/renan/App.java
@@ -0,0 +1,13 @@
+package dev.renan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
